6aeba61a96262f5a82222112242fb528f68305de
rkiarezouki <rkia.rezouki@etud.univ-jfc.fr>
2024-01-31 09:02:18 +0100

    push

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -1,29 +1,20 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {GraphqlService} from "./graphql.serive";
+import {Product, World} from ".world";
+import {BACKEND} from "./request";
+import { Component } from "@angular/core";
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class Appcomponent {
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have the 'FrontISISCAPITALIST' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontISISCAPITALIST');
-  });
+  world:World = new World()
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FrontISISCAPITALIST');
-  });
-});
+  constructor(services:GraphqlService){
+    services.getWorld().then(res => 
+      this.world = res.data.getWorld)
+  }
+}
---
+++ Frontend/src/app/request.ts
@@ -0,0 +1,3 @@
+import {gql} from '@urql/core'
+
+export const BACKEND = "http://localhost:4000/graphql"
